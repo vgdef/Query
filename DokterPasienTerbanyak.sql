d2c19d69b1e702b0dc83741c911378e870f43c17
vgdef <dbasirsleimena@gmail.com>
2023-05-30 13:45:08 +0900

    Merge remote-tracking branch 'origin/master'

--- DokterPasienTerbanyak.sql
+++ DokterPasienTerbanyak.sql
@@ -1,5 +1,7 @@
 SELECT
  `master`.getNamaLengkapPegawai(d.NIP) DPJP
+     , d.NIP
+     ,smf.ID
  , smf.DESKRIPSI SMF
  , COUNT(*) KUNJUNGAN
  
@@ -7,6 +9,7 @@ FROM pendaftaran.kunjungan k
 JOIN `master`.dokter d ON d.ID=k.DPJP
 JOIN `master`.dokter_smf ds ON ds.DOKTER=d.ID
 JOIN `master`.referensi smf ON smf.ID=ds.SMF AND smf.JENIS=26
-WHERE k.MASUK BETWEEN '2023-01-01' AND '2023-01-31'
+WHERE k.MASUK BETWEEN '2023-01-01' AND '2023-03-31'
+AND ds.STATUS=1 AND d.STATUS=1 AND ds.SMF!=31 AND smf.STATUS=1
 GROUP BY d.ID
 ORDER BY KUNJUNGAN DESC
